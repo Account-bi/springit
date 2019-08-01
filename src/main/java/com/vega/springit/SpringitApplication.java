6a50c38bf96db58859fddd7abf2111be24215cdd
mbo <mbo@bi-web.de>
2019-08-01 17:01:31 +0200

    added properties

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,15 +1,18 @@
 package com.vega.springit;
 
+import com.vega.springit.config.SpringitProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
   public static void main(String[] args) {
 
     SpringApplication.run(SpringitApplication.class, args);
-    System.out.println("Welcome to this super dope application!!!");
+
   }
 
 }
---
+++ src/main/java/com/vega/springit/config/SpringitProperties.java
@@ -0,0 +1,22 @@
+package com.vega.springit.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit") // der prefix springit kann in dem property file verwendet werden
+public class SpringitProperties {
+
+  /**
+   * This is the welcome message
+   */
+
+  private String welcomeMsg = "Guten Tag!";
+
+  public String getWelcomeMsg() {
+    return welcomeMsg;
+  }
+
+  public void setWelcomeMsg(String welcomeMsg) {
+    this.welcomeMsg = welcomeMsg;
+  }
+}
