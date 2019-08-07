09c270887bb08861fc2a058b5302e59215396a54
mbo <mbo@bi-web.de>
2019-08-07 15:02:07 +0200

    added application production props file

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,24 +1,15 @@
 package com.vega.springit;
 
 import com.vega.springit.config.SpringitProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
-  @Autowired
-  private SpringitProperties springitProperties;
 
-  @Autowired
-  private ApplicationContext applicationContext;
 
   public static void main(String[] args) {
 
@@ -27,14 +18,6 @@ public class SpringitApplication {
 
   }
 
-  @Bean
-  @Profile("dev")
-  CommandLineRunner runner(){
-      return args -> {
-        System.out.println("Printing out all the bean names in the application context");
-        System.out.println("----------------------------------------------------------");
-        String[] beans = applicationContext.getBeanDefinitionNames();
-      };
-  }
+
 
 }
