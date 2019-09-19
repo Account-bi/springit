f7914272b3a055060c31b35c07907385790a325d
mbo <mbo@bi-web.de>
2019-09-19 14:29:19 +0200

    added rememberMe (63)

--- src/main/java/com/vega/springit/security/SecurityConfiguration.java
+++ src/main/java/com/vega/springit/security/SecurityConfiguration.java
@@ -37,7 +37,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .loginPage("/login").permitAll()
             .usernameParameter("email")
             .and()
-        .logout();
+        .logout()
+            .and()
+        .rememberMe();
+
   }
 
   @Override
