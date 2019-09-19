3e52eff83801f3ad894e41a03024a672e7e2587b
mbo <mbo@bi-web.de>
2019-09-19 14:09:02 +0200

    added login (61)

--- src/main/java/com/vega/springit/security/SecurityConfiguration.java
+++ src/main/java/com/vega/springit/security/SecurityConfiguration.java
@@ -21,18 +21,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
-        .requestMatchers(EndpointRequest.to("info")).permitAll()
-        .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
-        .antMatchers("/actuator/").hasRole("ACTUATOR")
-        .antMatchers("/link/submit").hasRole("USER")
-        .antMatchers("/link/**").permitAll()
-        .antMatchers("/").permitAll()
-        .antMatchers("/h2-console/**").permitAll()
+            .requestMatchers(EndpointRequest.to("info")).permitAll()
+            .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+            .antMatchers("/actuator/").hasRole("ADMIN")
+            .antMatchers("/").permitAll()
+            .antMatchers("/link/submit").hasRole("USER")
+            .antMatchers("/h2-console/**").permitAll()
         .and()
         .formLogin()
-        .and()
-        .csrf().disable()
-        .headers().frameOptions().disable();
+            .loginPage("/login").permitAll()
+            .usernameParameter("email");
+//        .and()
+//            .csrf().disable()
+//            .headers().frameOptions().disable();
   }
 
   @Override
